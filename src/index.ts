d63f39dc1159573e3cac618484c0bc308ce4d73a
Devin Davis <devin@warriertech.com>
2024-12-28 12:31:40 -0500

    update manifest names

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 
@@ -6,37 +6,37 @@ const app = express();
 app.use(express.json());
 
 // Example routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to the Node.js TypeScript API!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the Node.js TypeScript API!");
 });
 
 // CRUD routes
 // Get all items
-app.get('/items', (req: Request, res: Response) => {
-  res.json({ message: 'Returning all items', data: [] });
+app.get("/items", (req: Request, res: Response) => {
+  res.json({ message: "Returning all items", data: [] });
 });
 
 // Get a specific item
-app.get('/items/:id', (req: Request, res: Response) => {
+app.get("/items/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   res.json({ message: `Returning item with id ${id}`, data: { id } });
 });
 
 // Create an item
-app.post('/items', (req: Request, res: Response) => {
+app.post("/items", (req: Request, res: Response) => {
   const { name } = req.body;
-  res.status(201).json({ message: 'Item created', data: { id: 1, name } });
+  res.status(201).json({ message: "Item created", data: { id: 1, name } });
 });
 
 // Update an item
-app.put('/items/:id', (req: Request, res: Response) => {
+app.put("/items/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   const { name } = req.body;
   res.json({ message: `Item with id ${id} updated`, data: { id, name } });
 });
 
 // Delete an item
-app.delete('/items/:id', (req: Request, res: Response) => {
+app.delete("/items/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   res.json({ message: `Item with id ${id} deleted` });
 });
@@ -48,4 +48,3 @@ app.listen(PORT, () => {
 });
 
 export default app;
-

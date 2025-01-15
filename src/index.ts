aeb638d90a0ddfec4e3b8876f60c7ed31c693e82
devin <devin@mailfence.com>
2025-01-15 15:09:45 -0500

    remove database code from index file

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,11 @@
 import express, { Request, Response } from "express";
-import "dotenv/config";
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-// Docs: https://sequelize.org/docs/v6/getting-started/
-import { Sequelize } from "sequelize";
-
-const user = process.env.MYSQL_USER;
-const pass = process.env.MYSQL_PASSWORD;
-const host = "node-ts-api-mysql";
-const port = 3306;
-const dbname = process.env.MYSQL_DATABASE;
-
-// Replace user, pass, and dbname with your actual credentials
-const sequelize = new Sequelize(
-  `mysql://${user}:${pass}@${host}:${port}/${dbname}`,
-);
-
-// Example routes
-app.get("/", async (req: Request, res: Response) => {
-  try {
-    await sequelize.authenticate();
-
-    res.send("Connection has been established successfully.");
-  } catch (error) {
-    res.send("Unable to connect to the database.");
-  }
-});
-
-app.get("/db_variables", (req: Request, res: Response) => {
-  res.send(`${user}:${pass}@${host}:${port}/${dbname}`);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the Node.js TypeScript API!");
 });
 
 // Start server

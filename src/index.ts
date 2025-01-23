b1532deb13c8b442551ef065f3cb4750839a44c7
devin <devin@mailfence.com>
2025-01-23 16:04:42 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Node.js TypeScript API!");
 });
 
+app.get("/flux", (req: Request, res: Response) => {
+  res.send("Welcome to the Node.js TypeScript API!");
+});
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
